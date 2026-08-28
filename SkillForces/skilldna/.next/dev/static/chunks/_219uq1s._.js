688f94209332370968b3f90cd70dd856f0640e8e
satyamkumar25923 <satyamkumar25923@gmail.com>
2026-08-29 01:09:42 +0530

    Remove .next build artifacts from commit

--- SkillForces/skilldna/.next/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__146d930._.css","static/chunks/node_modules_1w3izq-._.js","static/chunks/src_0d4395n._.js","static/chunks/src_components_1uas943._.js","static/chunks/node_modules_1t57em8._.js"],
-    source: "entry"
-});
